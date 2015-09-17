6f7fe5432c086cf49c4a4ff0f79b13f24198fddb
Demid Merzlyakov <demid@Air-Demid.Dlink>
2015-09-18 00:13:03 +0500

    Initial Commit

---
+++ LoadMethodTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoadMethodTest
+//
+//  Created by Demid Merzlyakov on 18.09.15.
+//  Copyright (c) 2015 FreeNickname. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LoadMethodTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoadMethodTest
+//
+//  Created by Demid Merzlyakov on 18.09.15.
+//  Copyright (c) 2015 FreeNickname. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
